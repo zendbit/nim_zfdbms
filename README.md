060dcac84e2c10822e7ab98a6d25026d08c08677
Amru Rosyada <amru.rosyada@gmail.com>
2021-09-27 22:37:17 +0700

    update readme

--- README.md
+++ README.md
@@ -14,5 +14,35 @@ https://github.com/zendbit/nim.zfdbms.git
 #### usage
 available plugins
 ```
+##
+##  database connection
+##  database : mysql
+##  database name: test
+##  database user: admin
+##  database password: 12345
+##
+
 import zfdbms/dbms
+
+const DEFAULT_DATETIME_FORMAT* = "yyyy-MM-dd HH:mm:ss"
+const DEFAULT_DATE_FORMAT* = "yyyy-MM-dd"
+
+var dbi {.global.}: DBMS[MySql] = @[newDBMS[MySql]("sinacc")]
+
+proc dbConn(connType: DbConnType): DBMS[MySql] {.gcsafe.} =
+  # disable gcsafe checking
+  {.cast(gcsafe).}:
+    let conn = ord(connType)
+    if dbi[conn].isNil or not dbi[conn].ping:
+      echo "try connect to database: " &
+        $dbi[conn].tryConnect
+
+    result = dbi[conn]
+
+proc createUID*(): string =
+  let dtime = now().utc().format("yyyy-MM-dd HH:mm:ss:fffffffff")
+  result = $($dtime).secureHash
+
+proc sinaccDb*(): DBMS[MySql] {.gcsafe.} =
+  result = dbConn(Sinacc)
 ```
--- zfdbms/dbms.nim
+++ zfdbms/dbms.nim
@@ -1,10 +1,10 @@
 ##
-##  zfcore web framework for nim language
+##  zfdbms is dbms tools for generate, connect and query tools
 ##  This framework if free to use and to modify
 ##  License: BSD
 ##  Author: Amru Rosyada
 ##  Email: amru.rosyada@gmail.com
-##  Git: https://github.com/zendbit/nim.zfplugs
+##  Git: https://github.com/zendbit/nim.zfdbms
 ##
 
 const WITH_MYSQL* = defined(mysqldb) or defined(nimdoc)
@@ -28,7 +28,7 @@ when WITH_MYSQL or WITH_PGSQL or WITH_SQLITE:
 
   export options, strutils, sequtils, json, strformat
 
-  import stdext/[json_ext, strutils_ext, system_ext], dbs, settings, dbssql
+  import stdext/[json_ext, strutils_ext, system_ext], dbs, dbssql
   export dbs, dbssql, json_ext, strutils_ext, system_ext
 
   type
@@ -204,59 +204,6 @@ when WITH_MYSQL or WITH_PGSQL or WITH_SQLITE:
     onDelete: string = FK_CASCADE,
     onUpdate: string = FK_CASCADE,
     columnRef: string = "") {.pragma.}
-
-  ##  var db: DBConn
-  ##
-  ##  this will read the settings.json on the section
-  ##  "database": {
-  ##    "your_connId_setting": {
-  ##      "username": "",
-  ##      "password": "",
-  ##      "database": "",
-  ##      "host": "",
-  ##      "port": 1234
-  ##    }
-  ##  }
-  ##
-  proc newDBMS*[T](connId: string): DBMS[T] {.gcsafe.} =
-    ##
-    ##  create new DBMS object (database connection)
-    ##  pass connId from settings.json in database section
-    ##  let myconn = newDBMS[MySql]("zcms")
-    ##
-    let jsonSettings = jsonSettings()
-    if not jsonSettings.isNil:
-      let db = jsonSettings{"database"}
-      if not db.isNil:
-        let dbConf = db{connId}
-        if not dbConf.isNil:
-          result = DBMS[T](
-            connId: connId,
-            dbmsStack: initTable[DbmsStmtType, DbmsFieldType]())
-          result.dbInfo = (
-            dbConf{"database"}.getStr(),
-            dbConf{"username"}.getStr(),
-            dbConf{"password"}.getStr(),
-            dbConf{"host"}.getStr(),
-            dbConf{"port"}.getInt())
-          let c = newDbs[T](
-            result.dbInfo.database,
-            result.dbInfo.username,
-            result.dbInfo.password,
-            result.dbInfo.host,
-            result.dbInfo.port).tryConnect()
-
-          result.connected = c.success
-          if c.success:
-            result.conn = c.conn
-          else:
-            echo c.msg
-
-        else:
-          echo &"database {connId} not found!!."
-
-      else:
-        echo "database section not found!!."
 
   proc newDBMS*[T](
     database: string,
